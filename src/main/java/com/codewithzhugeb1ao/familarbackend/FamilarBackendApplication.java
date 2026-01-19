c217d766106ca766bbdc1af4e2214b5e12fc2e5f
Đoàn Huỳnh Gia Bảo <153264378+ZhugeB1ao@users.noreply.github.com>
2026-01-19 09:56:27 +0700

    Initial project

---
+++ src/main/java/com/codewithzhugeb1ao/familarbackend/FamilarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codewithzhugeb1ao.familarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilarBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewithzhugeb1ao/familarbackend/FamilarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithzhugeb1ao.familarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
